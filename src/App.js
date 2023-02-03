f702f82388f95a1935b012fe8d5942ff3ce817cc
ScottMartino <scott.martino@gmail.com>
2023-02-03 10:00:11 -0500

    API call and getting information into state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
+import {useEffect, useState}  from 'react'
+import axios from 'axios';
+import Form from './Form';
 
 function App() {
+  
+  const [minifigs, setMinifigs] = useState ([])
+  
+  
+  useEffect(()=>{
+    // Making my API call
+    axios({
+        url: 'https://rebrickable.com/api/v3/lego/minifigs/',
+        params: {
+          key: 'a4a279c7f86d60b0557cbc70201686e9',
+          search: 'pirate',
+          page_size: 10,
+        },
+      }).then((response)=>{
+        
+        // store data in state
+        const minifigresults = response.data.results
+        setMinifigs(minifigresults)
+        
+        }
+      )
+  }      
+, [])
+  
+  
+  
+  
+
+
+
+
+
   return (
     <div className="App">
       <h1>LEGO Minifigure Search</h1>
+      
+      <Form />
+      
+      {minifigs.map( (minfig)=>{
+        return(
+          <img src={minfig.set_img_url} alt="minfig" />
+        )
+      })
+
+    }
+      
+
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,13 @@
+const Form = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">Enter MiniFigure Name or Type</label>
+                <input type="text" />
+                
+            </form>
+        </div>
+    )
+}
+
+export default Form
