e57d1589504fbcb99d5611f66793744682dd7c6a
ScottMartino <scott.martino@gmail.com>
2023-02-07 14:50:26 -0500

    error handling

--- src/Components/Displayphotos.js
+++ src/Components/Displayphotos.js
@@ -7,7 +7,14 @@ const Displayphotos = (props) => {
             return (  
                 <div key={ index }
                     className="returnbox" >
+                    
+                    
+                    
                     <img src = {minifig.set_img_url ? minifig.set_img_url : './assets/stock.jpg'} alt={minifig.name}/>
+                    
+                    
+                    
+                    
                     <p>{minifig.name}</p>
                     <a href={minifig.set_url} target="_blank" rel="noreferrer" className="rebricklink">Rebrickable Link</a>
                 </div>
